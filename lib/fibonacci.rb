e1ba432daf847f7599d47ce25f278390fc751c41
Mark Skelton <markskelton10@gmail.com>
2024-10-01 13:26:18 +0100

    Create recursive instance method fibs_rec
    Includes testing.

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -10,6 +10,21 @@ class Fibonacci
 
     2.upto(num - 1).reduce([0, 1]) { |fib_arr, fib_num| fib_arr << fib_arr[fib_num - 1] + fib_arr[fib_num - 2] }
   end
+
+  def fibs_rec(num)
+    return [] if num.zero?
+    return [0] if num == 1
+    return [0, 1] if num == 2
+
+    fib_arr = fibs_rec(num - 1)
+    fib_arr << fib_arr[num - 2] + fib_arr[num - 3]
+  end
 end
 
+puts 'Iterative Fibonacci:'
 10.times { |num| pp Fibonacci.new.fibs(num) }
+
+puts ''
+
+puts 'Recursive Fibonacci:'
+10.times { |num| pp Fibonacci.new.fibs_rec(num) }
