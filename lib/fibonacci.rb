f78765701977c8d5dace7d2ceb65866edb4fd97b
Mark Skelton <markskelton10@gmail.com>
2024-10-01 12:57:36 +0100

    Create fibs iterative class method
    Included creating the Fibonacci class and testing.

---
+++ lib/fibonacci.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+# Fibonacci has methods to return an array of the first n fibonacci numbers using an
+# iterative approach and using a recursive approach
+class Fibonacci
+  def self.fibs(num)
+    return [] if num.zero?
+    return [0] if num == 1
+    return [0, 1] if num == 2
+
+    2.upto(num - 1).reduce([0, 1]) { |fib_arr, fib_num| fib_arr << fib_arr[fib_num - 1] + fib_arr[fib_num - 2] }
+  end
+end
+
+10.times { |num| pp Fibonacci.fibs(num) }
